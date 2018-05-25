335b1c2e54b9832969e5e9ef39b3fa1b31c9c95f
Thuong <truonghoaithuong2000@gmail.com>
2018-05-25 10:22:28 +0700

    add component folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './header/Header';
-import SideBar from './sidebar/SideBar';
-import Contact from './contact/Contact';
-import Login from './login/Login';
-import DataTable from './datatable/DataTable';
+import Header from './components/header/Header';
+import SideBar from './components/sidebar/SideBar';
+import Contact from './components/contact/Contact';
+import Login from './components/login/Login';
+import DataTable from './components/datatable/DataTable';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
